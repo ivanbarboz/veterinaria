1ce5c152a05deeed146058137d597d4a2f843144
Alexandra Vegas <alexvegasdev@gmail.com>
2024-09-02 23:37:18 -0500

    feat: :sparkles: agregar documentos de usuarios

--- database/seeders/UserDocumentSeeder.php
+++ database/seeders/UserDocumentSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\DocumentType;
+use App\Models\User;
+use App\Models\UserDocument;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,24 @@ class UserDocumentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $documents = [
+            ['user_id' => 1, 'document_type_id' => 1, 'document_number' => '87452106'], // DNI
+            ['user_id' => 1, 'document_type_id' => 2, 'document_number' => '12345678901'], // RUC
+            ['user_id' => 2, 'document_type_id' => 1, 'document_number' => '87654321'], // DNI
+            ['user_id' => 2, 'document_type_id' => 3, 'document_number' => '12345678901234567890'], // Pasaporte
+            ['user_id' => 3, 'document_type_id' => 2, 'document_number' => '23456789012'], // RUC
+            ['user_id' => 4, 'document_type_id' => 1, 'document_number' => '34567890'], // DNI
+            ['user_id' => 5, 'document_type_id' => 3, 'document_number' => '23456789012345678901'], // Pasaporte
+            ['user_id' => 6, 'document_type_id' => 2, 'document_number' => '45678901234'], // RUC
+            ['user_id' => 7, 'document_type_id' => 1, 'document_number' => '56789012'], // DNI
+            ['user_id' => 8, 'document_type_id' => 3, 'document_number' => '34567890123456789012'], // Pasaporte
+            ['user_id' => 9, 'document_type_id' => 2, 'document_number' => '67890123456'], // RUC
+            ['user_id' => 10, 'document_type_id' => 1, 'document_number' => '78901234'], // DNI
+        ];
+
+        foreach ($documents as $document) {
+            UserDocument::create($document);
+        }
     }
+
 }
